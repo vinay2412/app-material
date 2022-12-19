8ce54123ce37b76b0072ff21d6549d9dcf7deb0b
Vinod Prajapati <73247049+vinay2412@users.noreply.github.com>
2022-12-19 08:52:34 +0530

    Update app.module.ts
    Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,6 +8,12 @@ import { HomeComponent } from './component/home/home.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button'
+import {MatDividerModule} from '@angular/material/divider'
+import{MatStepperModule} from '@angular/material/stepper'
+import {MatExpansionModule} from '@angular/material/expansion'
+import{MatListModule} from '@angular/material/list'
 
 @NgModule({
   declarations: [
@@ -22,7 +27,14 @@ import {MatCardModule} from '@angular/material/card';
     MatToolbarModule,
     MatIconModule,
     MatCardModule,
-    BrowserAnimationsModule
+    MatGridListModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatListModule,
+    MatStepperModule,
+    MatButtonModule
   ],
   providers: [
   ],
